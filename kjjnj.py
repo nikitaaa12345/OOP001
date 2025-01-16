bd5ee1dca0f8f2b19462d929d1718d48457416aa
Nikita Vozny <nikita.vozny@ventspils.edu.lv>
2025-01-16 09:03:04 +0200

    1

--- kjjnj.py
+++ kjjnj.py
@@ -1,10 +1,26 @@
 class Dzivnieki:
     def __init__(self, vards,kajas):
-        self.name=vards
+        self.vards=vards
+        self.kajas=kajas
     def skanja(self):
         print("random animal noise")
+    def __str__(self):
+        return f"{self.vards} un {self.kajas} kajas"
+
+
+class Suns(Dzivnieki):
+    def __init__(self,vards,kajas):
+        super().__init__(vards,kajas)
+        self.vards="Komisars "+self.vards
+    def skanja(self):
+        print("Vau vau")
 
 d1=Dzivnieki("Gauja",4)
+print(d1)
 d1.skanja()
 
+
+s1=Suns("Reksis",4)
+print(s1)
+s1.skanja()
 
